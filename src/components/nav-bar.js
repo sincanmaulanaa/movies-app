fc3940ced8c58ae33bf3e3488f4526a9089a31c5
sincanmaulanaa <sincanmaulanaa@gmail.com>
2022-10-08 00:31:32 +0800

    update ui

--- src/components/nav-bar.js
+++ src/components/nav-bar.js
@@ -6,7 +6,7 @@ class NavBar extends HTMLElement {
 
   render() {
     this.innerHTML = `
-	<nav class="bg-gray-700 flex text-white justify-center align-items-center space-x-6 p-4">
+	<nav class="z-50 bg-gray-700 flex text-white justify-center align-items-center space-x-6 p-4 fixed inset-x-0">
 		<a href="">Home</a>
 		<a href="">Popular</a>
 		<a href="">Category</a>
