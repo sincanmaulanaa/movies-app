b7f7f0bd0256e6cf516255711f09d070f4711211
Sincan Maulana <sincanmaulanaa@gmail.com>
2022-10-14 02:28:39 +0800

    fix some bugs

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -68,8 +68,7 @@ function main() {
     value = document.querySelector("#search-element").value;
 
     if (value.length == 0) {
-      console.log(value);
-      return alert("Masukkan film yang ingin kamu cari!");
+      return showResponseMessage("Film yang kamu cari tidak tersedia.");
     } else {
       return value;
     }
@@ -81,6 +80,11 @@ function main() {
     results.forEach((result) => {
       searchResults.innerHTML += `
       <div class="max-w-sm bg-white rounded-lg my-8 mx-5 border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700 w-1/5">
+      <a href="#">
+              <img class="rounded-t-lg object-cover" src="${imageBaseUrl}/original/${
+        result.poster_path
+      }" alt="">
+            </a>
       <div class="p-5">
         <a href="#">
           <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">${
@@ -150,7 +154,7 @@ function main() {
             )}....</p>
             <a href="#" class="inline-flex items-center py-2 px-3 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
               Read more
-              <svg aria-hidden="true" class="ml-2 -mr-1 w-4 h-4" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+            <svg aria-hidden="true" class="ml-2 -mr-1 w-4 h-4" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
             </a>
           </div>
 		    </div>
@@ -159,8 +163,13 @@ function main() {
     });
   };
 
+  // const cancelButton = () => {
+  //   const formContainer = document.querySelector("#form-container");
+
+  // };
+
   document
-    .querySelector("#searchButton")
+    .querySelector("#search-button")
     .addEventListener("click", function (e) {
       e.preventDefault();
       const movieList = document.querySelector("#movie-list");
@@ -172,10 +181,19 @@ function main() {
       findMovie();
     });
 
+  document
+    .querySelector("#search-button")
+    .addEventListener("click", function () {
+      const searchButton = document.querySelector("#search-button");
+      let cancel = document.createElement("button");
+      cancel.setAttribute("id", "cancel-button");
+      cancel.innerText = "Cancel";
+
+      searchButton.replaceWith(cancel);
+    });
+
   getTopRatedMovies();
   getPopularMovies();
 }
 
 export default main;
-
-391090000873422;
